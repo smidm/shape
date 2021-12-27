d7c7ab91a7148677d98e0cf1995affb8c4782413
Matěj Šmíd <m@matejsmid.cz>
2021-12-27 06:53:15 +0100

    add metadata and documentation to setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,58 @@
 #!/usr/bin/env python
 # coding=utf-8
+import io
+import re
+from os.path import dirname, join
 
 from setuptools import setup
+
+
+def read(*names, **kwargs):
+    with io.open(
+            join(dirname(__file__), *names),
+            encoding=kwargs.get('encoding', 'utf8')
+    ) as fh:
+        return fh.read()
+
 
 setup(
     name='shapes',
     version='0.1',
+    license='MIT',
     description='2D shapes abstraction',
+    long_description=re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.md')),
+    long_description_content_type='text/markdown',
     author='Matěj Šmíd',
     author_email='m@matejsmid.cz',
-#    url='https://github.com/bbcrd/audio-offset-finder',
-#    license='Apache License 2.0',
+    url='https://github.com/smidm/shapes',
     packages=['shapes'],
     install_requires=[
         'opencv-python-headless',
         'numpy',
         'matplotlib',
     ],
+    python_requires='>=3.6',
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Utilities',
+    ],
+    project_urls={
+        # 'Changelog': 'https://github.com/smidm/shapes/blob/master/CHANGELOG.md',
+        'Issue Tracker': 'https://github.com/smidm/shapes/issues',
+    },
+    zip_safe=True,
 )
-
